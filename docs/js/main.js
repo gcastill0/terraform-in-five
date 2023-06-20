f4b4c80249b8cda2cc844e33737fda7f6d04bd65
gcastill0 <gilberto@hashicorp.com>
2023-06-20 12:16:20 -0400

    Refactored code and structure

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,3 +1,46 @@
+const parser = new DOMParser();
+
+async function fetchSVG(svg_name) {
+    const response = await fetch(svg_name)
+    const svgText = await response.text()
+    const svgDoc = parser.parseFromString(svgText, 'text/xml')
+
+    return svgDoc
+}
+
+async function typingEffect(element, text, speed) {
+    var i = 0;
+    var c = 0;
+
+    var timer = setInterval(function () {
+        if (i < text.length) {
+            var ch = text.charAt(i);
+            if (ch === '\n') {
+                element.append(ch);
+                c = 0;
+            } else if (ch === '\\') {
+                element.append(ch);
+                linebreak = document.createElement("br");
+                element.appendChild(linebreak);
+                c = 0;
+            }
+            else if (((c > 56 && c < 65) && ch === ' ') || c >= 65) {
+                element.append(ch);
+                linebreak = document.createElement("br");
+                element.appendChild(linebreak);
+                c = 0;
+            }
+            else {
+                element.append(ch);
+            }
+            i++;
+            c++;
+        } else {
+            clearInterval(timer);
+        }
+    }, speed)
+}
+
 window.onload = function () {
 
     const menu_items = document.querySelectorAll('.menu-item');
@@ -26,6 +69,64 @@ window.onload = function () {
             }
         })
     })
+
+
+    const hud_play_button = document.getElementById("hud-play-button")
+    const hud_terminal = document.getElementById("hud-terminal")
+
+    let console_display = {};
+
+    document.querySelectorAll(".bash").forEach((item) => {
+        console_display[item.id] = item.innerHTML;
+        item.innerHTML = "";
+    });
+
+    document.querySelectorAll(".json").forEach((item) => {
+        console_display[item.id] = item.innerHTML;
+        item.innerHTML = "";
+    });
+
+    let hud_display_handler = function () {
+        if (hud_terminal.getAttribute("active") == "true") return;
+
+        hud_play_button.setAttribute("active", "false")
+        hud_terminal.setAttribute("active", "true")
+        playStage("step01", "step02");
+
+    }
+
+    hud_play_button.addEventListener('click', hud_display_handler)
+
+    if ('ontouchstart' in window) {
+        menu.addEventListener("touchstart", function () {
+            var touchHndl = function () {
+                hud_display_handler();
+                this.removeEventListener(touchHndl)
+            }
+            this.addEventListener(touchHndl);
+        });
+    }
+
+    function playStage(command_id, response_id) {
+        let command = document.querySelector("#" + command_id);
+        let response = document.querySelector("#" + response_id);
+
+        command.innerHTML = "";
+        response.innerHTML = "";
+
+        let command_text = String(console_display[command_id]).replace(/(\s{2,})/g, " ");
+        let response_text = String(console_display[response_id]).replace(/([\n\r])/g, "NL").replace(/(\s{2,})/g, " ").replace(/NL/g, "\n");
+
+        let delay = command_text.length * 20 + 500;
+
+        typingEffect(command, command_text, 50);
+
+        function playResponse() {
+            typingEffect(response, response_text, 0);
+        }
+
+        setTimeout(playResponse, delay);
+    }
 
     // fetchSVG("plan_description.svg").then(svgDoc => {
     //     svg_diagram.appendChild(svgDoc.documentElement)
